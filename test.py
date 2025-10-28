9f61578219d1f309c29b12edbc9e08540d83e5cd
esraa079154-oss <esraa079154@gmail.com>
2025-10-28 08:40:16 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import pandas as pd
 df = pd.DataFrame(
     [
         { "task": "" ,"Done?":False} ])
-class Table:
+class introduction:
     st.image(r"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMYjHrjikYhnIvKdjygdlHAYVCjwi1ZrXI9A&s")
     #header
     st.header("To_do_list")
@@ -14,21 +14,20 @@ class Table:
         st.write(f"Welcome {user_name} ")
     st.subheader("The plan ")
 st.image(r"https://emojiat.com/assets/img/emoji/1f4cb.png")
-    def __init__(self, data):
-        #st.image(r"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMYjHrjikYhnIvKdjygdlHAYVCjwi1ZrXI9A&s")
-        
-        self.data = data
-        if "data_df" not in st.session_state:
-              st.session_state.data_df = pd.DataFrame(
-        [{ "task": '' ,"Done?":False } ])
-    df=pd.DataFrame(df)
+    
  
     def print_table(self):
       pass
         
 class Table:
+    
     def __init__(self, data):
+    #st.image(r"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMYjHrjikYhnIvKdjygdlHAYVCjwi1ZrXI9A&s")
         self.data = data
+    if "data_df" not in st.session_state:
+          st.session_state.data_df = pd.DataFrame(
+    [{ "task": '' ,"Done?":False } ])
+    df=pd.DataFrame(df)
 
     def print_table(self):
         for row in self.data:
@@ -42,6 +41,7 @@ my_table = Table(table_data)
 # # # # # # طباعة الجدول
 
 my_table.print_table()
+
 
 
 
