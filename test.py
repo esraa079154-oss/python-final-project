a0d1dff4ada6fac2217f317a9b1222d9096dad1b
esraa079154-oss <esraa079154@gmail.com>
2025-10-28 07:17:32 -0700

    Update test.py

--- test.py
+++ test.py
@@ -15,17 +15,15 @@ class Table:
         for row in self.data:
             # طباعة كل خلية في الصف مع ترك مسافة
             print(" | ".join(map(str, row)))
-table_data = [
-    ["الاسم", "العمر", "المدينة"],
-    ["أحمد", 30, "الرياض"],
-    ["سارة", 25, "جدة"],
-    ["علي", 35, "الدمام"]
-]
+table_data =  [{ "task": '' ,"Done?":False } ]
 
 # # # # # إنشاء كائن من الفئة Table
 my_table = Table(table_data)
 
 # # # # # طباعة الجدول
+if "data_df" not in st.session_state:
+    st.session_state.data_df = pd.DataFrame(
+        [{ "task": '' ,"Done?":False } ])
 my_table.print_table()
 
 
